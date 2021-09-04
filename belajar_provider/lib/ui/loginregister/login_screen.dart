3a76c08c1af8f9299e1da8267dc7209b42a82ec8
RIzki Syaputra <rizki@udacoding.com>
2021-09-04 13:51:02 +0700

    update login screen

--- belajar_provider/lib/ui/loginregister/login_screen.dart
+++ belajar_provider/lib/ui/loginregister/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:belajar_provider/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
 
@@ -9,29 +11,47 @@ class LoginScreen extends StatelessWidget {
         title: Text('Login Page'),
       ),
 
-      body: Center(
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                labelText: 'Email'
+      body: Consumer<AuthProvider>(
+        builder: (BuildContext context, value, Widget child){
+          return Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 20
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: value.email,
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        labelText: 'Email'
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  TextFormField(
+                    controller: value.password,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Password',
+                        labelText: 'Password'
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  value.loadingLogin ? CircularProgressIndicator() :
+                  MaterialButton(onPressed: (){
+                    if(value.loadingLogin) return;
+                    value.login(context);
+                  },
+                    child: Text("Login"),
+                    color: Colors.red,
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 16,),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: 'Password',
-                  labelText: 'Password'
-              ),
-            ),
-            MaterialButton(onPressed: (){},
-            child: Text("Login"),
-              color: Colors.red,
-            ),
-          ],
-        ),
+          );
+        },
+
       ),
     );
   }
