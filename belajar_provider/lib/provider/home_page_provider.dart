7896e1e16791d6d0edbc45623f56a4985adc43f9
RIzki Syaputra <rizki@udacoding.com>
2021-09-04 11:50:09 +0700

    add home screen

---
+++ belajar_provider/lib/provider/home_page_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePageProvider extends ChangeNotifier{
+
+  List<String> users = [];
+
+  //method logic
+  Future<void> getListUser(){
+    
+  }
+}
---
+++ belajar_provider/lib/ui/homepage/home_page_screen.dart
@@ -0,0 +1,38 @@
+import 'package:belajar_provider/provider/home_page_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+//setstate di ganti dengan notifylisternet pada provider
+
+class HomeScreen extends StatefulWidget {
+
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => HomePageProvider(),
+      
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('List Users'),
+        ),
+        
+        body: Consumer<HomePageProvider>(
+          builder: (BuildContext context,value, Widget? child){
+           return ListView.builder(
+              itemCount: users.lenght,
+              itemBuilder: (context, index){
+                return Text(users{index});
+              },
+            ),
+          },
+        )
+      ),
+    );
+  }
+}
