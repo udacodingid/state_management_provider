e8c2c4b2a8655c108e6f1e69a199c304b82dcbae
RIzki Syaputra <rizki@udacoding.com>
2021-09-04 14:09:48 +0700

    update page home list users

--- belajar_provider/lib/provider/home_page_provider.dart
+++ belajar_provider/lib/provider/home_page_provider.dart
@@ -1,17 +1,41 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePageProvider extends ChangeNotifier{
 
   List<String> users = [];
+  bool loading = false;
 
   //method logic
-  Future<void> getListUser() async{
-
-    await Future.delayed(Duration(seconds: 5));
-
-    users = ['Serr', 'asasa', 'asasa'];
-
+  Future<void> getListUser(BuildContext context) async{
+    loading = true;
     notifyListeners();
 
+    http.Response res = await http.post(Uri.parse('https://reqres.in/api/users?page=2'));
+
+    loading = false;
+    notifyListeners();
+
+    if(res.statusCode == 200){
+
+      var dataUser = jsonDecode(res.body);
+      users = dataUser['data'];
+      notifyListeners();
+
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error")));
+    }
   }
+
+  // //method logic
+  // Future<void> getListUser() async{
+  //
+  //   await Future.delayed(Duration(seconds: 5));
+  //
+  //   users = ['Serr', 'asasa', 'asasa'];
+  //
+  //   notifyListeners();
+  //
+  // }
 }
